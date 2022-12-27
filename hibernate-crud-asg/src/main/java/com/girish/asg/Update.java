3c82b8daeb6c4f4f87b08514293a657cad220822
Girish Giradkar <girishgiradkar824@gmail.com>
2022-12-27 10:05:04 +0530

    hibernate-crud-push

---
+++ hibernate-crud-asg/src/main/java/com/girish/asg/Update.java
@@ -0,0 +1,29 @@
+package com.girish.asg;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+
+public class Update {
+
+	public static void main(String[] args)
+	{
+		Configuration cfg = new Configuration();
+		cfg.configure();
+		StandardServiceRegistryBuilder ssrb = new StandardServiceRegistryBuilder();
+		ssrb.applySettings(cfg.getProperties());
+		StandardServiceRegistry ssr = ssrb.build();
+		SessionFactory sf = cfg.buildSessionFactory(ssr);
+		Session s = sf.openSession();
+		Transaction t =s.beginTransaction();
+		Student st = new Student(22,"Harshal","JalgaonPorya");
+		s.update(st);
+		t.commit();
+		s.close();
+		sf.close();
+	}
+}
